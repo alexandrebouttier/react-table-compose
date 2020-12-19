3808c17106f979bce3f6b191fe487fbff22c2b51
Alexandre Bouttier <abouttier@linaia.com>
2020-12-19 16:44:32 +0100

    add debug

--- src/components/EditModal.jsx
+++ src/components/EditModal.jsx
@@ -17,6 +17,7 @@ const EditModal = ({ fields, data, nameEntities, dataId, editMethod }) => {
   const toggleModalEdit = () => setShowModalEdit(!showModalEdit)
 
   const submitEdit = (e) => {
+    console.log('submitEdit()')
     console.log('e', e)
 
     return editMethod(e).then((res) => toggleModalEdit())
