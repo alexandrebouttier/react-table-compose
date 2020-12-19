d1682ae9a900f1ecc275122288a2d38cef1a4663
Alexandre Bouttier <abouttier@linaia.com>
2020-12-19 14:34:12 +0100

    add substring options fieldText et prefix

--- src/components/fields/FieldText.jsx
+++ src/components/fields/FieldText.jsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import { upperFirst, upperCase, head } from 'lodash';
+import React from 'react'
+import { upperFirst, upperCase, head } from 'lodash'
 const FieldText = ({ options, field, data, index }) => {
   const renderField = (options) => {
     if (options) {
       switch (head(Object.keys(options))) {
         case 'upperFirst':
-          return upperFirst(data[field]);
+          return upperFirst(data[field])
         case 'upperCase':
-          return upperCase(data[field]);
+          return upperCase(data[field])
+        case 'substring':
+          return data[field].substring(
+            options.substring.maxLength || 3,
+            data[field].length
+          )
         default:
       }
     }
     if (options && options.transformValue) {
-      return options.transformValue[data[field]];
+      return options.transformValue[data[field]]
     } else {
-      return data[field];
+      return data[field]
     }
-  };
+  }
 
-  return <td key={`field-badge${index}`}>{renderField(options)}</td>;
-};
+  return (
+    <td key={`field-badge${index}`}>
+      {renderField(options)}
+      {(options.prefix && options.prefix) || ''}
+    </td>
+  )
+}
 
-export default FieldText;
+export default FieldText
